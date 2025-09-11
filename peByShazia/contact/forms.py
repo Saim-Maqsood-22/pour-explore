c6856ed1728258df5f610950ffb335d27aebebfe
Saim Maqsood <maqsoodsaim2002@gmail.com>
2025-09-11 15:58:11 +0500

    style the contact form

--- peByShazia/contact/forms.py
+++ peByShazia/contact/forms.py
@@ -6,3 +6,23 @@ class contactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ["customer_name", "customer_email", "message"]
+        labels={
+            'customer_name':'Your Name',
+            'customer_email':'Your Email',
+            'message':'Your Message'
+        }
+        widgets={
+            'customer_name':forms.TextInput(attrs={
+                'class':'w-screen p-3 bg-white outline-none',
+                'placeholder':'Enter your name'
+            }),
+            'customer_email':forms.EmailInput(attrs={
+                'class':'w-screen p-3 bg-white outline-none',
+                'placeholder':'Enter your E-mail'
+            }),
+            'message':forms.Textarea(attrs={
+                'class':'w-screen p-3 bg-white outline-none',
+                'placeholder':'Enter your message',
+                'rows':2,
+            }),
+        }
--- peByShazia/shop/models.py
+++ peByShazia/shop/models.py
@@ -5,7 +5,7 @@ class products(models.Model):
     image=models.ImageField(upload_to='media')
     name=models.CharField(max_length=20)
     description=models.CharField(max_length=50)
-    price=models.PositiveBigIntegerField(null=False)
+    price=models.PositiveIntegerField(null=False)
 
     class Meta:
         verbose_name='product'
